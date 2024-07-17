8ff8184d534345ebc5d1445ac7141cd999688156
Jorge Sardon <anibal317@gmail.com>
2024-07-17 20:17:01 -0300

    adding libraries

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 8000,
+  pageLoadTimeout: 8000,
+  log: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-    specPattern:['cypress/e2e/tests/**/*.{ts,js}']
+    specPattern: ['cypress/e2e/tests/**/*.{ts,js}']
   },
 });
