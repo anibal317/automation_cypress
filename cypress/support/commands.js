a970fa8a2b554afcb078be17d52282e8c576608c
Jorge Sardon <anibal317@gmail.com>
2024-08-13 15:59:51 -0300

    adding new comands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,53 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Intercepts the new tab oppening
+Cypress.Commands.add('noNewTab', function () {
+    cy.window().then((win) => {
+        cy.stub(win, 'open').callsFake((url) => {
+            // Oppening in the same window
+            win.location.href = url;
+        });
+    });
+})
+
+
+Cypress.Commands.add('waitLoading', function (timeOut, interval) {
+    cy.waitUntil(() =>
+        commonElements.loadingIcon().then($el => $el.prop('hidden') === true),
+        {
+            timeout: timeOut || 20000, // tiempo máximo de espera en milisegundos
+            interval: interval || 500   // intervalo entre intentos en milisegundos
+        }
+    )
+})
+
+Cypress.Commands.add('getSelectors', function (container) {
+    // Seleccionar el contenedor
+    cy.get(container).then($container => {
+        const container = $container[0];
+        const elements = container.querySelectorAll('*');
+        const uniqueSelectors = new Set();
+
+        elements.forEach(el => {
+            if (el.id) {
+                uniqueSelectors.add(`#${el.id}`);
+            }
+            if (el.className) {
+                el.classList.forEach(cls => uniqueSelectors.add(`.${cls}`));
+            }
+            const tagName = el.tagName.toLowerCase();
+            uniqueSelectors.add(tagName);
+        });
+
+        // Mostrar los selectores únicos en la consola de Cypress
+        console.log([...uniqueSelectors].join('\n'));
+    });
+
+})
+Cypress.Commands.add('getContainerText', (container) => {
+    cy.get(container).each($container => {
+        console.log(`'${$container.text().trim()}',`)
+    });
+})
